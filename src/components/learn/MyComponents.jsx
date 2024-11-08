9798637f75f597138724ca0328f47a6040f1f698
Dat Nguyen <terrydatnguyen@gmail.com>
2024-11-08 11:21:00 +0700

    video 33

--- src/components/learn/MyComponents.jsx
+++ src/components/learn/MyComponents.jsx
@@ -1,9 +1,17 @@
 //fragment
 import "./style.css";
 const MyComponent = () =>{
+    // const hoidanit = "terry";
+    // const hoidanit = {
+    //     name: "dat",
+    //     age: 16
+    // };
+    const hoidanit=[1,2,3]
     return (
       <>
-        <div>dat and locus va thanh</div>
+        {/* <div>{hoidanit.name} and locus va thanh</div> */}
+        <div>{JSON.stringify(hoidanit)} and locus va thanh</div>
+        <div>{console.log("Terry")}</div>
         <div className="child">child</div>
       </>
       
