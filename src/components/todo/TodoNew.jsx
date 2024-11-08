5c726d1d223523629f56a83d2751e54134e08758
Dat Nguyen <terrydatnguyen@gmail.com>
2024-11-08 15:23:43 +0700

    video 35

---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,10 @@
+const TodoNew =()=>
+{
+    return(
+        <div className="todo-new">
+            <input type="text" placeholder="Enter Your Task" />
+            <button>add</button>
+        </div>
+    );
+}
+export default TodoNew;
