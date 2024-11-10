2d15c2f7e2dd658416aa4d559d1bb299055e9954
Dat Nguyen <terrydatnguyen@gmail.com>
2024-11-10 14:10:23 +0700

    39

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,10 +2,16 @@ const TodoNew =(props)=>{
     console.log(">>> check point:",props)
     const {addNewTodo}=props;
     // addNewTodo("terry")
+    const handleClick = ()=>{
+        alert("click me")
+    }
+    const handleOnChange=(name)=>{
+        console.log(">>> handleOnChange",name)
+    }
     return(
         <div className="todo-new">
-            <input type="text" placeholder="Enter Your Task" />
-            <button>add</button>
+            <input type="text" placeholder="Enter Your Task" onChange={(event)=>handleOnChange(event.target.value)} />
+            <button onClick={handleClick}>add</button>
         </div>
     );
 }
